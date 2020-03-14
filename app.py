7014a5c4b4f50cd5226c55bc4903a0630ef4f274
Harshak777 <Harshak777@users.noreply.github.com>
2020-03-14 19:47:45 +0530

    Ulti

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ NEXT_BUTTON_XPATH = '//input[@type="submit" and @id="SLOGIN"]'
 driver.implicitly_wait(2)
 time.sleep(2)
 
+print('Starting Webpage title: ',driver.title)
+
 #positive student login
 driver.find_element_by_id('studentLoginBtn').click()
 driver.implicitly_wait(2)
@@ -28,8 +30,61 @@ driver.find_element_by_xpath(NEXT_BUTTON_XPATH).submit()
 driver.implicitly_wait(3)
 time.sleep(3)
 
-if "Timetable companion" == driver.title:
-    print('Test Passed')
+print('Page changed to: ',driver.title)
+if "Student Timetable companion" == driver.title:
+    print('Student Login Test Passed')
 else:
-    print('Test Failed')
+    print('Student Login Test Failed')
 
+driver.quit()
+
+driver = webdriver.Chrome('C:/Webdriver/chromedriver.exe',chrome_options=options)
+
+driver.get('http://localhost/files/')
+NEXT_BUTTON_XPATH = '//button[@type="submit" and @id="viewsemester"]'
+
+driver.implicitly_wait(2)
+time.sleep(2)
+
+driver.find_element_by_id('teacherLoginBtn').click()
+driver.implicitly_wait(2)
+time.sleep(2)
+driver.find_element_by_name('FN').send_keys("test@gmail.com")
+driver.implicitly_wait(2)
+time.sleep(2)
+driver.find_element_by_name('password').send_keys("123456789")
+driver.implicitly_wait(2)
+time.sleep(2)
+driver.find_element_by_id('FLOGIN').submit()
+
+if "Teacher Timetable companion" == driver.title:
+    print('Teacher Login Test Passed')
+else:
+    print('Teacher Login Test Failed')
+
+driver.quit()
+
+driver = webdriver.Chrome('C:/Webdriver/chromedriver.exe',chrome_options=options)
+
+driver.get('http://localhost/files/')
+NEXT_BUTTON_XPATH = '//input[@type="submit" and @id="ALOGIN"]'
+
+driver.implicitly_wait(3)
+time.sleep(3)
+
+driver.find_element_by_id('adminLoginBtn').click()
+driver.implicitly_wait(1)
+time.sleep(1)
+driver.find_element_by_name('UN').send_keys("admin")
+driver.implicitly_wait(1)
+time.sleep(1)
+driver.find_element_by_id('password').send_keys("pass123")
+driver.implicitly_wait(1)
+time.sleep(1)
+driver.find_element_by_xpath(NEXT_BUTTON_XPATH).submit()
+
+if 'Admin Timetable companion' == driver.title:
+    print('Teacher Login Test Passed')
+else:
+    print('Teacher Login Test Failed')
+    
