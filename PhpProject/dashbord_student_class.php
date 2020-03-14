2eeca1d98227562c3e8a7a8c74a142351ef5ba4a
Harshak777 <Harshak777@users.noreply.github.com>
2020-03-14 19:41:57 +0530

    Pen

--- PhpProject/dashbord_student_class.php
+++ PhpProject/dashbord_student_class.php
@@ -29,7 +29,7 @@ $sid=$_SESSION['sid'];
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <title>Timetable companion</title>
+  <title>Student Timetable companion</title>
   <!-- Bootstrap core CSS-->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <!-- Custom fonts for this template-->
