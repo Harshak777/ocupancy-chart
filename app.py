e2d4a9fbac3a7d790582bb08f5f8a45469fc1e03
Harshak777 <Harshak777@users.noreply.github.com>
2020-03-14 17:40:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-import selenium
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import Select
+
+driver = webdriver.Chrome('C:/Webdriver/chromedriver.exe')
+
 print('hi')
