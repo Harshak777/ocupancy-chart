b0ada7f6f15850e4bd2ce9b8046edc757a685d04
Harshak777 <Harshak777@users.noreply.github.com>
2020-03-15 02:56:42 +0530

    s

--- PhpProject/allot.php
+++ PhpProject/allot.php
@@ -19,27 +19,28 @@ $tid=$_SESSION['tid'];
         $row=mysqli_fetch_array($run_query);
         $fn=$row['faculty_number'];
 
-
-$qq = mysqli_query($conn, "SELECT * from subjects WHERE  allotedto = '$fn'");
+$qqs = "SELECT * from subjects WHERE  allotedto = '$fn'";
+$qq = mysqli_query($conn,$qqs);
 $row=mysqli_fetch_array($qq);
 $subc=$row['subject_code'];
 $subn=$row['subject_name'];
 $sub= $subc."<br>".$subn; 
 //"UPDATE $select_classroom SET $select_period = 'NEWSUB' WHERE day = $select_day"
 //SELECT * FROM $select_classroom WHERE period1 = '-<br>-' and day = '$select_day' and $select_period =  'period1'
-$q1 = mysqli_query($conn, "SELECT $select_period FROM $select_classroom WHERE  $select_period = '-<br>-' and day = '$select_day'");
+$q1s =  "SELECT $select_period FROM $select_classroom WHERE  $select_period = '-<br>-' and day = '$select_day'";
+$q1 = mysqli_query($conn,$q1s);
 $row = mysqli_fetch_array($q1);
  if($row)
  {
-    
-     $q = mysqli_query($conn, "UPDATE $select_classroom SET $select_period = '$sub' WHERE day = '$select_day'");
+    $upd =  "UPDATE $select_classroom SET $select_period = '$sub' WHERE day = '$select_day'";
+     $q = mysqli_query($conn,$upd);
      if ($q) {
         $sql = "SELECT * FROM sections WHERE class_name = '$select_classroom'";
         $run_query=mysqli_query($conn,$sql);
         $row=mysqli_fetch_array($run_query);
         $s_name = $row['sec_name'];
-        
-        $qs1 = mysqli_query($conn, "INSERT INTO notification (sec_name,croom_no,notify) VALUES('$s_name','$select_classroom','Timetable Updated<br>New Period Added<br>Check Your Room TT')");
+        $in = "INSERT INTO notification (sec_name,croom_no,notify) VALUES('$s_name','$select_classroom','Timetable Updated<br>New Period Added<br>Check Your Room TT')";
+        $qs1 = mysqli_query($conn, $in);
          
         $message = "Allotted";
          echo "<script type='text/javascript'>alert('$message');</script>";
